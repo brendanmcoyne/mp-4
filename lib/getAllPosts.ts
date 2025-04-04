0394ed80a08a8e1a50153283f788e4ab44da5574
brendanmcoyne <bmcoyne@bu.edu>
2025-04-04 16:03:58 -0400

    Changes

--- lib/getAllPosts.ts
+++ lib/getAllPosts.ts
@@ -5,7 +5,17 @@ export default async function getAllPosts(): Promise<ExhibitProps[]> {
     const API_URL = `https://api.harvardartmuseums.org/exhibition?apikey=${API_KEY}`;
 
     const response = await fetch(API_URL);
+
+
+    if (!response.ok) {
+        throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
+    }
+
     const data = await response.json();
+
+    if (!data.records) {
+        throw new Error("Invalid API response: 'records' is missing or not an array.");
+    }
 
     return data.records.map((p: ExhibitProps): ExhibitProps => ({
         exhibitionid: p.exhibitionid,
